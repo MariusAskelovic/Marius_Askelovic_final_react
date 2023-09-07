2aa9a1612fe8ee2734bab0ba7704b31ae2d32e25
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-09-07 15:53:57 +0300

    RegisterPage template

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -4,11 +4,12 @@ export default function LoginPage() {
   return (
     <div className='container text-neutral-300 text-center py-20'>
       <h2 className='text-2xl'>Welcome to our page</h2>
-      <h1 className='text-7xl pt-4 pb-7 text-neutral-100 font-semibold'>
-        Shops
-      </h1>
-      <p>Login to enter</p>
-      <GoogleAuth />
+      <h1 className='text-8xl pt-4 pb-7 text-neutral-100 font-bold '>Shops</h1>
+      <p className='text-lg'>Login to enter</p>
+      <div>
+        <h4>Alternative Login Methods:</h4>
+        <GoogleAuth />
+      </div>
     </div>
   );
 }
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,3 +1,33 @@
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <div className='container py-14 px-10 '>
+      <h3 className='text-white text-xl text-center mb-3'>
+        Create New Account
+      </h3>
+      <div className='bg-white py-4 px-3 text-center rounded-xl'>
+        <form className='inline-flex flex-col items-center'>
+          <input
+            className='py-1 px-2 border rounded-md mb-3'
+            type='email'
+            placeholder='email'
+          />
+          <input
+            className='py-1 px-2 border rounded-md mb-3'
+            type='password'
+            placeholder='password'
+            id='password'
+          />
+          <input
+            className='py-1 px-2 border rounded-md mb-5'
+            type='password'
+            placeholder='confirm password'
+            id='confirmPassword'
+          />
+          <button className='border border-neutral-300 rounded-md bg-gray-100 px-4 py-1 text-xs text-neutral-600 uppercase font-semibold'>
+            Register
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
