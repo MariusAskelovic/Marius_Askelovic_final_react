d1c0a40b68db41377a621b87bb989340d4b5ddf7
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-09-07 11:03:48 +0300

    Routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.VITE_ENV_apiKey,
   authDomain: import.meta.VITE_ENV_authDomain,
@@ -13,5 +9,4 @@ const firebaseConfig = {
   appId: import.meta.VITE_ENV_appId,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
