2f491e8695b25ee6d5313f725f8b797310b6556f
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-09-08 13:53:29 +0300

    ShopsPage working

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -1,8 +1,34 @@
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
+import { useEffect, useState } from 'react';
+
 export default function Shops() {
+  const [dbData, setDbData] = useState([]);
+
+  async function getDbData() {
+    const querySnapshot = await getDocs(collection(db, 'shops'));
+    const tempArr = [];
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+      tempArr.push(doc.data());
+    });
+    console.log('tempArr ===', tempArr);
+    setDbData(tempArr);
+  }
+
+  useEffect(() => {
+    getDbData();
+  }, []);
   return (
     <div>
-      <h1>TEMPORARY SHOPS.JSX</h1>
-      <ul></ul>
+      <h1 className='text-3xl text-white'>TEMPORARY SHOPS.JSX</h1>
+      <ul>
+        {dbData.map((sObj) => (
+          <li key={sObj.shopName}>
+            <img src={sObj.imageUrl} alt='alt' />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
