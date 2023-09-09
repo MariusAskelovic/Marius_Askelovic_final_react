96e26010cbda5fb5dedd45ceda2dc05176a21dd8
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-09-09 12:26:06 +0300

    Shops some styling

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -21,14 +21,22 @@ export default function Shops() {
     <div className='container'>
       <ul className='grid sm:grid-cols-2 lg:grid-cols-3'>
         {dbData.map((sObj) => (
-          <li key={sObj.id} className='bg-stone-900'>
-            <img
-              className='w-full object-cover aspect-square'
-              src={sObj.imageUrl}
-              alt={sObj.shopName}
-            />
-            <div>
-              <h4>{sObj.year}</h4>
+          <li
+            key={sObj.id}
+            className='bg-[#1f1f1f] mb-20 rounded-lg overflow-hidden'
+          >
+            <div className='w-full overflow-hidden'>
+              <img
+                className='w-full object-cover aspect-square hover:scale-110 transition duration-700'
+                src={sObj.imageUrl}
+                alt={sObj.shopName}
+              />
+            </div>
+            <div className='p-[30px] min-h-fit overflow-hidden'>
+              <h2 className='mt-[10px] mb-[15px] uppercase text-xs text-orange-600'>
+                {sObj.shopName}
+              </h2>
+              <h4 className='text-white truncate w-full'>{sObj.description}</h4>
             </div>
           </li>
         ))}
