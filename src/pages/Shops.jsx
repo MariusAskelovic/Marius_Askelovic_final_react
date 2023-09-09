9f18849978abaae6f2af79fb893257ef6c9fced2
MariusAskelovic <72157256+MariusAskelovic@users.noreply.github.com>
2023-09-09 12:49:27 +0300

    Group Hover

--- src/pages/Shops.jsx
+++ src/pages/Shops.jsx
@@ -23,16 +23,16 @@ export default function Shops() {
         {dbData.map((sObj) => (
           <li
             key={sObj.id}
-            className='bg-[#1f1f1f] mb-20 rounded-lg overflow-hidden'
+            className='bg-[#1f1f1f] mb-20 rounded-lg overflow-hidden group'
           >
             <div className='w-full overflow-hidden'>
               <img
-                className='w-full object-cover aspect-square hover:scale-110 transition duration-700'
+                className='w-full object-cover aspect-square group-hover:scale-110 transition duration-700'
                 src={sObj.imageUrl}
                 alt={sObj.shopName}
               />
             </div>
-            <div className='p-[30px] min-h-fit overflow-hidden'>
+            <div className='p-[30px] min-h-fit'>
               <h2 className='mt-[10px] mb-[15px] uppercase text-xs text-orange-600'>
                 {sObj.shopName}
               </h2>
